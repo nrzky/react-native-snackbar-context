0224007b32470e21f0ce6ae43863839a9bd0d1f7
Onur Özkaya <onur.ozkaya@macellan.net>
2022-08-31 00:23:36 +0300

    Fix SnackbarProvider prop types

--- src/types/Context.ts
+++ src/types/Context.ts
@@ -3,5 +3,14 @@ import type { ColorPalette } from './Helpers';
 
 export interface SnackbarProviderProps {
   colorPalette?: ColorPalette;
-  customAnimation?: CustomAnimation;
+  duration?: SnackbarProps['defaultDuration'];
+  animation?: SnackbarProps['defaultAnimation'];
+  position?: SnackbarProps['defaultPosition'];
+  spaces?: SnackbarProps['spaces'];
+  style?: SnackbarProps['style'];
+  textProps?: SnackbarProps['textProps'];
+  textStyle?: SnackbarProps['textStyle'];
+  indicatorStyle?: SnackbarProps['indicatorStyle'];
+  isVisibleTimer?: SnackbarProps['isVisibleTimer'];
+  customAnimation?: SnackbarProps['customAnimation'];
 }
